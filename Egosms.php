72aad450bb78a2fb51795632b38861f908dfdfaf
Ampeire <ampeireedgar5@gmail.com>
2022-03-03 09:38:03 +0300

    Initial Commit

---
+++ Egosms.php
@@ -0,0 +1,36 @@
+<?php
+
+
+
+
+/**
+ * @package Egosms
+ * @author Ampeire Edgar
+ * @license GPL V2 or later
+ */
+
+/*
+ * Plugin Name: Egosms
+ * Plugin URI: https://github.com/dancan-edgar/egosms-plugin.git
+ * Description: Datero enables use of shotcode  to display dates on a wordpress blog.
+ * Version: 1.0.0
+ * Author: Ampeire Edgar
+ * Author URI: https://github.com/dancan-edgar
+ * License: GPL V2 or higher
+ * Text Domain: egosms
+ */
+
+/*
+Egosms is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Egosms is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Datero. If not, see https://www.gnu.org/licenses/old-licenses/gpl-2.0.html.
+*/
